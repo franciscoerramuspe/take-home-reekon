f952f3dd64b2daaca022d94840c116c984825ad7
Francisco Erramuspe <franciscoerramuspe91@gmail.com>
2024-12-21 13:40:44 -0300

    test

--- server/app.js
+++ server/app.js
@@ -6,12 +6,16 @@ import organizationRoutes from './routes/organizationRoutes.js';
 import robotRoutes from './routes/robotRoutes.js';
 import errorRoutes from './routes/errorRoutes.js';
 import jobRoutes from './routes/jobRoutes.js';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(cors());
 app.use(express.json());
@@ -21,6 +25,7 @@ app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello World from Robot Control Backend!' });
 });
 
+app.use(express.static(__dirname + '/public/'));
 app.use('/api/auth', authRoutes);
 app.use('/api/organizations', organizationRoutes);
 app.use('/api/robots', robotRoutes);
